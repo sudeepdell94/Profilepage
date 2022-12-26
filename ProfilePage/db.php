ec9a38f97e47c390a0aa4788869b42e9293612f2
sudeepdell94 <83568415+sudeepdell94@users.noreply.github.com>
2022-12-26 10:59:41 +0530

    Delete ProfilePage directory

--- ProfilePage/db.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$mysql_hostname = "localhost";
-$mysql_user = "root";
-$mysql_password = "";
-$mysql_database = "profile";
-$con = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password, $mysql_database);
-?>
